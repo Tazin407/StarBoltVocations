eb8bd2806111f69ba3629178c392c63d61189132
tazin407 <tazintasnim34@gmail.com>
2024-03-09 21:54:49 +0600

    added csrf

--- StarBoltVocations/settings.py
+++ StarBoltVocations/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env('SECRET_KEY')
-
+CSRF_TRUSTED_ORIGINS = ['https://starboltvocations.onrender.com/','https://*.127.0.0.1']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
